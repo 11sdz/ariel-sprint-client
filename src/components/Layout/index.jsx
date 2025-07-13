93a38055bf81401c75b1ddc0deb1473be023a70a
shaked40 <shaked@getuna.ai>
2025-07-13 17:40:32 +0300

    pull nuvbar

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar';
+import styles from './style.module.scss';
 
 
 export default function Layout() {
   return (
     <div>
         <Navbar/>
-        <main>
+        <main className={styles.mainContent}>
             <Outlet/>
         </main>
     </div>
