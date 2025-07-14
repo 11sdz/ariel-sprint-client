f21a6750a16c79f846f3da7e9b565c7406491bf4
shaked40 <shaked@getuna.ai>
2025-07-14 12:01:11 +0300

    Merge branch 'dev' of https://github.com/11sdz/ariel-sprint-client into dev

--- src/components/Profile/ProfileImage.jsx
+++ src/components/Profile/ProfileImage.jsx
@@ -9,7 +9,6 @@ export default function ProfileImage({ fullName }) {
   
 return (
   <span
-    style={{width:width, height:height, fontSize:fontSize, backgroundImage: bgGradient}}
     className={styles.circleImage}
     aria-label={`Profile initials ${getInitials(fullName)}`}
   >
